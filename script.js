4c399de8c1af6606e09862a40099ba2243504bf9
Harshita Mathur <73068872+harshita1712@users.noreply.github.com>
2023-10-25 12:14:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// JavaScript code
+
+
+var search=document.getElementById('search');
+
+
+search.addEventListener('focus',(event)=>{
+
+  document.getElementById('search-wrapper').style.border="1px solid #00395d";
+
+});
+
+
+search.addEventListener('focusout',(event)=>{
+
+document.getElementById('search-wrapper').style.border="1px solid rgba(0, 0, 0, 0.276)";
+
+});
+
+let hostnames = [];
+let options = ['Chef', 'Data', 'Patching', 'Change','Incident', 'Configuration'];
+
+function addHostname() {
+    const input = document.getElementById("search");
+    const hostname = input.value.trim();
+
+    if (hostname) {
+        hostnames.push(hostname);
+        input.value = "";
+
+        displayOptions();
+    }
+}
+
+function displayOptions() {
+    const optionsList = document.getElementById("optionsList");
+    optionsList.style.display = "block";
+
+    const optionsUl = document.getElementById("options");
+    optionsUl.innerHTML = "";
+
+    for (let i = 0; i < options.length; i++) {
+        const option = options[i];
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.value = option;
+        checkbox.addEventListener("change", () => selectOption(option, checkbox.checked));
+
+        const label = document.createElement("label");
+        label.textContent = option;
+        label.appendChild(checkbox);
+
+        const listItem = document.createElement("li");
+        listItem.appendChild(label);
+
+        optionsUl.appendChild(listItem);
+    }
+}
+
+function selectOption(option, isSelected) {
+    if (isSelected) {
+        hostnames.push(option);
+    } else {
+        const index = hostnames.indexOf(option);
+        if (index !== -1) {
+            hostnames.splice(index, 1);
+        }
+    }
+
+    // Check if any individual checkbox is unchecked
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const selectAllCheckbox = document.getElementById("selectAll");
+    
+    for (const checkbox of checkboxes) {
+        if (checkbox !== selectAllCheckbox && !checkbox.checked) {
+            selectAllCheckbox.checked = false;
+            return; // Exit the loop
+        }
+    }
+}
+function toggleCheckboxes(selectAllCheckbox) {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach(checkbox => {
+        if (checkbox !== selectAllCheckbox) {
+            checkbox.checked = selectAllCheckbox.checked;
+        }
+    });
+}
